313f37766f31d52fc6dd5b7bf28fbb4bacb05df6
Ainatche <Ainatche@6334b9af-0850-4c29-903e-b08ada63b9ef>
2012-05-24 09:37:16 +0000

    Added file names to send to status page and discard empty file name.

--- frame/trunk/application/controllers/importer.php
+++ frame/trunk/application/controllers/importer.php
@@ -6,4 +6,44 @@ class Importer extends Controller{
 		
 		$this->load->view('importer');		
 	}
+	
+	function upload(){	
+		$numOfFiles = count($_FILES['files']['tmp_name']);
+		
+		$base_dir ="D:/uploads/"; 
+		$name = $_COOKIE['edgebi_user']!='' ? $_COOKIE['edgebi_user'] : '000';
+		$target_dir = $base_dir .$name."_".rand()."/";
+		
+		if(mkdir($target_dir , 0777)) 
+		{ 	
+			$data = array();
+			
+			for($i=0; $i <$numOfFiles;$i++)
+			{
+				 $filename =  $_FILES['files']['name'] [$i];
+				if (!empty($filename))
+				{
+					$move_dir = $target_dir . basename( $filename); 
+					move_uploaded_file($_FILES['files']['tmp_name'][$i], $move_dir);
+					$data["_guids"][$i]=  $this->edgeapi->Request('/accounts/1007/Services/ImporterTester?options=a=b','POST', true, array('Content-Length: 0'));
+					$data["_files"][$i]=$filename;	
+				}							
+			}
+			
+			$this->load->view('status_page',$data);
+		} 
+		else 
+		{ 
+			//do something on error
+		}		
+	}
+	
+	function getStatus($guid){
+		$status =$this->edgeapi->Request('/accounts/1007/Services?guid='.$guid);  
+		
+		$data = array(
+	 	"status"=>$status
+	 		 );
+		echo  $data["status"]; 
+	}
 }
