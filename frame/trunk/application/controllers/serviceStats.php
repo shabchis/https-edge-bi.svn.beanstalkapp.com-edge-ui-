f159efabe5a3b8c044c2a2248a814e3de0169989
Ainatche <Ainatche@6334b9af-0850-4c29-903e-b08ada63b9ef>
2012-05-31 08:27:51 +0000

    New service statistics page

---
+++ frame/trunk/application/controllers/serviceStats.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * serviceStats controller, retrieves list of services and their statisitics for the requested day
+ */
+class serviceStats extends Controller{
+	
+	function index(){
+		
+		$this->load->view('serviceStats');		
+	}
+	
+	function  getServices(){
+		
+		$date = date('Y-m-d', strtotime($this->input->post('date')));
+		$account = $this->input->post('accountID');
+		$response = $this->edgeapi->Request('/accounts/'.$account.'/Services?date='.$date);
+		echo  $response;
+	}
+}
+
+?>
