9a270803f3ada42a10e6d6da9e04838ae86909eb
Ainatche <Ainatche@6334b9af-0850-4c29-903e-b08ada63b9ef>
2012-07-11 09:06:01 +0000

    Edited page so it looks better (No changes to code)

--- frame/trunk/application/controllers/main.php
+++ frame/trunk/application/controllers/main.php
@@ -6,29 +6,23 @@ class Main extends Controller {
 	{
 		// Get user data
 		$user = $this->edgeapi->Request('/users/'.get_cookie('edgebi_user'));
-		
-		
-		
+
 		// Get menu data
 		global $MENU_JSON;
 		$menu = $MENU_JSON;
-	
-		
+			
 		// Get account data
-	$accounts = $this->edgeapi->Request('/accounts');  
-		
-	//	$this->firephp->log(json_decode($accounts));
+		$accounts = $this->edgeapi->Request('/accounts');  
+
+		//$this->firephp->log(json_decode($accounts));
 		$data = array(
 			"menu"=>$menu,
 			"account"=>	stripslashes($accounts),
 			"user"=>$user
 			);
-	//	$this->firephp->log(json_decode($data["menu"]));
-		$this->load->view('includes/template',$data);	
-		 
 		
-			
-  }
-
+		//$this->firephp->log(json_decode($data["menu"]));
+		$this->load->view('includes/template',$data);	 					
+	  }
 }
 
