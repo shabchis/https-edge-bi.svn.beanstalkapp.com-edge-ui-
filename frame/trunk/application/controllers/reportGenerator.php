04af52059584d28185ace7c2259694dfb227ef67
Ainatche <Ainatche@6334b9af-0850-4c29-903e-b08ada63b9ef>
2012-07-12 11:22:06 +0000

    Report generator page (not finished). Supposed to support the option of generating custom reports per account (like EF Weekly Report)

---
+++ frame/trunk/application/controllers/reportGenerator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  ReportGenerator used to generate custom reports
+ */
+class reportGenerator extends Controller{
+	function index(){
+		
+		$this->load->view('reportGenerator');		
+	}
+}
+
+?>
