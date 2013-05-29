cafac949ffb00803058c0977b7811448d066ff40
doronassayas <doronassayas@6334b9af-0850-4c29-903e-b08ada63b9ef>
2013-05-29 15:41:14 +0000

    More refactoring

--- frame/trunk/application/errors/error_404.php
+++ frame/trunk/application/errors/error_404.php
@@ -1,24 +1,3 @@
 <?php header("HTTP/1.1 404 Not Found"); ?>
-
-<?php if (IS_AJAX): ?>
-<link rel="stylesheet" href="assets/css/errors.css" type="text/css" media="screen" />
-	
-	<div id="content">
-		<h1>Edge.BI Error</h1>
-		<h2><?php echo $heading; ?></h2>
-		<?php echo $message; ?>
-	</div>
-
-<?php else: ?>
-<link rel="stylesheet" href="assets/css/errors.css" type="text/css" media="screen" />
-
-  <body>
-  <div id="main">
-	<div id="content">
-	<h1>Edge.BI Error</h1>
-<h2><?php echo $heading; ?></h2>
-	<p><?php echo $message; ?></p>
-		</div>
-		</div>
-		</body>
-	<?php endif; ?>	
+<?php echo $message; ?>
+<?php exit(); ?>	
--- frame/trunk/application/errors/error_php.php
+++ frame/trunk/application/errors/error_php.php
@@ -1,15 +1,8 @@
-<div id="content">
-	<h1>Edge.BI Error</h1>
-	<h2>A PHP Error was encountered</h2>
-	
-	<p>Severity: <?php echo $severity; ?></p>
-	<p>Message:  <?php echo $message; ?></p>
-	<p>Filename: <?php echo $filepath; ?></p>
-	<p>Line Number: <?php echo $line; ?></p>
+<h2>A PHP Error was encountered</h2>
 
-</div>
- <link rel="stylesheet" href="assets/css/errors.css" type="text/css" media="screen" />
-<!--[if IE]>
-	<link rel="stylesheet" href="assets/css/ie_errors.css" type="text/css" media="screen" />
-		<![endif]-->
+<p>Severity: <?php echo $severity; ?></p>
+<p>Message:  <?php echo $message; ?></p>
+<p>Filename: <?php echo $filepath; ?></p>
+<p>Line Number: <?php echo $line; ?></p>
+
 <?php exit(); ?>	
