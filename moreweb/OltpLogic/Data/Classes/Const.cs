985a0bd53b13d4c6962751d67d29d500c1021fa4
doronassayas <doronassayas@6334b9af-0850-4c29-903e-b08ada63b9ef>
2013-06-05 16:14:04 +0000

    Fixed OltpLogicClient login error detection (ugly hack)

---
+++ moreweb/OltpLogic/Data/Classes/Const.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easynet.Edge.UI.Data
+{
+	public static class Const
+	{
+		public static string SessionExpiredMessage = "Session has expired. Please refresh the page.";
+	}
+}
