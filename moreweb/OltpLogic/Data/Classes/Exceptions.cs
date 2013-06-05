60f869784c482f2cbf914b8b4aea4da3f01da302
doronassayas <doronassayas@6334b9af-0850-4c29-903e-b08ada63b9ef>
2013-06-05 13:51:50 +0000

    Added excpetion details for session problems

---
+++ moreweb/OltpLogic/Data/Classes/Exceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easynet.Edge.UI.Data
+{
+	[Serializable]
+	public class EdgeSessionException : Exception
+	{
+		public EdgeSessionErrorType ErrorType { get; set; }
+
+		public EdgeSessionException() { this.ErrorType = EdgeSessionErrorType.Unspecified; }
+		public EdgeSessionException(string message, EdgeSessionErrorType errorType) : base(message) { this.ErrorType = errorType; }
+		public EdgeSessionException(string message, EdgeSessionErrorType errorType, Exception inner) : base(message, inner) { this.ErrorType = errorType; }
+		protected EdgeSessionException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context)
+			: base(info, context) {  }
+	}
+
+	public enum EdgeSessionErrorType
+	{
+		Unspecified,
+		BadFormat,
+		Expired,
+		NotFound,
+		InvalidUser
+	}
+}
